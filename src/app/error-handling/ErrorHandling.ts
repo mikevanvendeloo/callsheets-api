82f9695c74cb8e0628a17cb3a098ede5db93ecea
Mike van Vendeloo <mike.van.vendeloo@gmail.com>
2023-11-10 08:45:54 +0100

    basic api working

---
+++ src/app/error-handling/ErrorHandling.ts
@@ -0,0 +1,33 @@
+import { type NextFunction, type Request, type Response } from 'express'
+import { CustomError } from './CustomError'
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Response<never> => {
+  // Handled errors
+  if (err instanceof CustomError) {
+    const { statusCode, errors, logging } = err
+    if (logging) {
+      console.error(
+        JSON.stringify(
+          {
+            code: err.statusCode,
+            errors: err.errors,
+            stack: err.stack,
+          },
+          null,
+          2,
+        ),
+      )
+    }
+
+    return res.status(statusCode).send({ errors })
+  }
+
+  // Unhandled errors
+  console.error(JSON.stringify(err, null, 2))
+  return res.status(500).send({ errors: [{ message: 'Something went wrong' }] })
+}
